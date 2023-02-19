f7dd4bd5de1c4a10e0f53f538c895da0557ad220
Nicholas Breen <breenn@lmax.com>
2023-02-20 11:49:12 +1300

    fixup docker images for group 100 https://github.com/bazelbuild/bazel/issues/17535

--- images.sh
+++ images.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-docker build --rm -t rocky:8-rpmbuild images/rocky/8/rpmbuild
-docker build --rm -t centos:6-rpmbuild images/centos/6/rpmbuild
+docker build --rm --tag executor:debian - <<'EOF'
+FROM debian:stable-slim
+RUN getent group 100 | { IFS=: read group _ gid; groupdel $group; }
+EOF
+
+docker build --rm --tag executor:fedora - <<'EOF'
+FROM fedora:37
+RUN getent group 100 | { IFS=: read group _ gid; userdel games; groupdel $group; }
+EOF
+
+docker build --rm --tag executor:ubuntu - <<'EOF'
+FROM ubuntu:kinetic
+RUN getent group 100 | { IFS=: read group _ gid; groupdel $group; }
+EOF
+
+docker build --rm --tag executor:rocky images/rocky/8/rpmbuild
+docker build --rm --tag executor:centos images/centos/6/rpmbuild

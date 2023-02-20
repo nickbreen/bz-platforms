badb234155fbf7cf053f886e8a14819a1210eac1
nickbreen <nick@breen.kiwi>
2023-02-20 21:17:19 +1300

    clean up image.sh a little

--- images.sh
+++ images.sh
@@ -22,4 +22,19 @@ RUN yum install -y /usr/bin/rpmbuild /usr/bin/python3 && yum clean all
 RUN getent group 100 | { IFS=: read group _ gid; userdel games; groupdel $group; }
 EOF
 
-docker build --rm --tag executor:centos images/centos/6/rpmbuild
+docker build --rm --tag executor:centos --file - images/centos/6 <<'EOF'
+FROM centos:6
+
+RUN rm /etc/yum.repos.d/*
+
+# From https://www.getpagespeed.com/files/centos6-eol.repo
+# From https://www.getpagespeed.com/files/centos6-epel-eol.repo
+ADD centos6-eol.repo centos6-epel-eol.repo /etc/yum.repos.d/
+# From https://vault.epel.cloud/RPM-GPG-KEY-CentOS-6
+# From https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
+ADD RPM-GPG-KEY-CentOS-6 RPM-GPG-KEY-EPEL-6 /etc/pki/rpm-gpg/
+
+RUN yum install -y /usr/bin/rpmbuild /usr/bin/python3 && yum clean all
+
+RUN getent group 100 | { IFS=: read group _ gid; userdel games; groupdel $group; }
+EOF
